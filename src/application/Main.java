fe211aeeebf1d50ace4b10c8a1b60bcfd47684e4
Dylan Keskinyan <46177354+dylankeskinyan@users.noreply.github.com>
2019-01-02 16:02:11 -0500

    Uncommented Main

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,14 +8,7 @@ public class Main extends Application
 {
     public static void main(String[] args)
     {
-    	Compound nigga = new Compound("H2O");
-    	System.out.println(nigga.getFormula());
-    	System.out.println(nigga.getName());
-    	System.out.println(nigga.getElement1());
-    	System.out.println(nigga.getElement2());
-    	System.out.println(nigga.getQuantity1());
-    	System.out.println(nigga.getQuantity2());
-    	//launch(args);
+    	launch(args);
     }
 
     @Override
