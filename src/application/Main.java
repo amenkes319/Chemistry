58db1f16f46fac59c1e5669f22817c8e5b9548c5
Dylan Keskinyan <46177354+dylankeskinyan@users.noreply.github.com>
2019-01-19 00:49:14 -0500

    main update

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,5 @@
 package application;
 
-import java.io.IOException;
-import java.util.Arrays;
-
-import org.jsoup.select.Elements;
-
 import javafx.application.Application;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
